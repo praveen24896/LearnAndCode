9fe0f3b7ec6185eaa8873c8d25315736bf73b96c
Praveen Kumar <praveen.kumar@intimetec.com>
2024-04-03 21:46:34 +0530

    Added AccidentException class and refactored the code

--- Assignment5/CarAccidentScenario.java
+++ Assignment5/CarAccidentScenario.java
@@ -1,65 +1,62 @@
 public class CarAccidentScenario {
     public static void main(String[] args) {
         try {
-            throw new CabDriverInjuredException("Cab driver injured!");
-        } catch (CabDriverInjuredException e) {
-            System.out.println("Calling emergency services for injured cab driver...");
-            performEmergencyServices();
-        } finally {
+            handleAccident("CabDriver");
+        } catch (AccidentExceptions.CabDriverInjuredException e) {
+            System.out.println("Calling emergency services for injured driver...");
+            provideMedicalAssistance();
             contactFamily();
-        }
-
-        try {
-            throw new ScootyRiderInjuredException("Scooty rider injured!");
-        } catch (ScootyRiderInjuredException e) {
+        } catch (AccidentExceptions.ScootyRiderInjuredException e) {
             System.out.println("Calling emergency services for injured scooty rider...");
-            performEmergencyServices();
-        } finally {
+            provideMedicalAssistance();
             contactFamily();
-        }
-
-        try {
-            throw new PropertyDamageException("Property damage occurred!");
-        } catch (PropertyDamageException e) {
+        } catch (AccidentExceptions.PropertyDamageException e) {
             System.out.println("Handling property damage...");
             handlePropertyDamage();
-        } 
-
-        try {
-            throw new EnvironmentalDamageException("Environmental damage occurred!");
-        } catch (EnvironmentalDamageException e) {
+        } catch (AccidentExceptions.EnvironmentalDamageException e) {
             initiateEnvironmentalCleanup();
-        }
-
-        try {
-            throw new OtherPeopleInjuredException("Other people injured!");
-        } catch (OtherPeopleInjuredException e) {
+        } catch (AccidentExceptions.OtherPeopleInjuredException e) {
             provideMedicalAssistance();
-        } finally {
             contactFamily();
-        }
-
-        try {
-            throw new OthersPropertyDamageException("Others' property damage occurred!");
-        } catch (OthersPropertyDamageException e) {
+        } catch (AccidentExceptions.OthersPropertyDamageException e) {
             System.out.println("Contacting property owner and insurance company...");
             handlePropertyDamage();
-        } finally {
-            contactAuthorities();
-        }
-
-        try {
-            throw new FireCausedException("Fire caused by the accident!");
-        } catch (FireCausedException e) {
+        } catch (AccidentExceptions.FireCausedException e) {
+            precautionarySteps();
             initiateFirefighting();
             provideMedicalAssistance();
+        } catch (AccidentExceptions.UnknownAccidentException e) {
+            System.out.println("Unknown accident occurred!");
         } finally {
-            precautionarySteps();
+            contactAuthorities();
+            initiateEnvironmentalCleanup();
         }
     }
 
-    private static void performEmergencyServices() {
-        System.out.println("Emergency services dispatched. Providing medical assistance.");
+    private static void handleAccident(String type)
+            throws AccidentExceptions.CabDriverInjuredException, AccidentExceptions.ScootyRiderInjuredException,
+            AccidentExceptions.PropertyDamageException, AccidentExceptions.EnvironmentalDamageException,
+            AccidentExceptions.OtherPeopleInjuredException,
+            AccidentExceptions.OthersPropertyDamageException, AccidentExceptions.FireCausedException,
+            AccidentExceptions.UnknownAccidentException {
+        switch (type) {
+            case "CabDriver":
+                throw new AccidentExceptions.CabDriverInjuredException("Cab driver injured!");
+            case "ScootyRider":
+                throw new AccidentExceptions.ScootyRiderInjuredException("Scooty rider injured!");
+            case "PropertyDamage":
+                throw new AccidentExceptions.PropertyDamageException("Property damage occurred!");
+            case "EnvironmentalDamage":
+                throw new AccidentExceptions.EnvironmentalDamageException("Environmental damage occurred!");
+            case "OtherPeopleInjured":
+                throw new AccidentExceptions.OtherPeopleInjuredException("Other people injured!");
+            case "OthersPropertyDamage":
+                throw new AccidentExceptions.OthersPropertyDamageException("Others' property damage occurred!");
+            case "FireCaused":
+                throw new AccidentExceptions.FireCausedException("Fire caused by the accident!");
+            default:
+                throw new AccidentExceptions.UnknownAccidentException("Unknown accident type: " + type);
+        }
     }
 
     private static void handlePropertyDamage() {
@@ -67,19 +64,19 @@ public class CarAccidentScenario {
     }
 
     private static void initiateEnvironmentalCleanup() {
-        System.out.println("Environmental cleanup initiated. Contacting relevant authorities.");
+        System.out.println("Dispatch the crowd and initiate the environment cleanup.");
     }
 
     private static void provideMedicalAssistance() {
-        System.out.println("Providing first aid and arranging transportation to hospital.");
+        System.out.println("Providing first aid and arranging transportation of injured to the nearest hospital.");
+    }
+
+    private static void precautionarySteps() {
+        System.out.println("Evacuate the nearby area and perform necessary action to stop the spread of fire");
     }
 
     private static void initiateFirefighting() {
         System.out.println("Call Fire brigade and start the fire extinguishing efforts.");
-    }
-
-    private static void  precautionarySteps() {
-        System.out.println("Evacuate the nearby area and perform necessary action to stop the spread of fire");
     }
 
     private static void contactFamily() {
@@ -88,47 +85,5 @@ public class CarAccidentScenario {
 
     private static void contactAuthorities() {
         System.out.println("Contact the nearby authorities");
-    }
-}
-
-class CabDriverInjuredException extends Exception {
-    public CabDriverInjuredException(String message) {
-        super(message);
-    }
-}
-
-class ScootyRiderInjuredException extends Exception {
-    public ScootyRiderInjuredException(String message) {
-        super(message);
-    }
-}
-
-class PropertyDamageException extends Exception {
-    public PropertyDamageException(String message) {
-        super(message);
-    }
-}
-
-class EnvironmentalDamageException extends Exception {
-    public EnvironmentalDamageException(String message) {
-        super(message);
-    }
-}
-
-class OtherPeopleInjuredException extends Exception {
-    public OtherPeopleInjuredException(String message) {
-        super(message);
-    }
-}
-
-class OthersPropertyDamageException extends Exception {
-    public OthersPropertyDamageException(String message) {
-        super(message);
-    }
-}
-
-class FireCausedException extends Exception {
-    public FireCausedException(String message) {
-        super(message);
     }
 }
